785f0e695e4330d77bc4f097df7bef2d80312b59
István Juhos <stewemetal@gmail.com>
2024-12-20 23:27:08 +0100

    Add Day05_2

--- src/Day05.kt
+++ src/Day05.kt
@@ -22,7 +22,7 @@ fun main() {
 
         forEachIndexed { index, page ->
             val pagesShouldBeAfter = activeRules[page] ?: emptySet()
-            if(take(index).intersect(pagesShouldBeAfter).isNotEmpty()) return false
+            if (take(index).intersect(pagesShouldBeAfter).isNotEmpty()) return false
         }
 
         return true
@@ -37,18 +37,32 @@ fun main() {
         }
     }
 
+    fun List<Int>.correctedBy(rules: Map<Int, Set<Int>>): List<Int> =
+        sortedWith { o1, o2 ->
+            when {
+                rules[o1]?.contains(o2) ?: false -> -1
+                else -> 1
+            }
+        }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val rules = input.takeWhile { it.isNotBlank() }.asRules()
+        val prints = input.takeLastWhile { it.isNotBlank() }.asPrints()
+
+        return prints.filter {
+            !it.fits(rules)
+        }.sumOf {
+            it.correctedBy(rules).run { get(size / 2) }
+        }
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 143)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 123)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day05")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
-
